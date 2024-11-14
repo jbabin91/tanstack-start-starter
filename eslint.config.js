e9dbd6e519087f6f5e09a4fbfb431176bb4f0dfa
Jace Babin <jbabin91@gmail.com>
2024-11-13 17:11:45 -0700

    update eslint rules for tailwind
    Signed-off-by: Jace Babin <jbabin91@gmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -141,6 +141,9 @@ export default tseslint.config(
       react: {
         version: 'detect',
       },
+      tailwindcss: {
+        callees: ['cn', 'cva'],
+      },
     },
   },
   configPrettier,
