540d0a2aebf28c0f7d335c047c2a3dc2e8035882
Jace Babin <jbabin91@gmail.com>
2024-11-20 18:18:18 -0700

    updates to app config
    Signed-off-by: Jace Babin <jbabin91@gmail.com>

--- app.config.ts
+++ app.config.ts
@@ -7,14 +7,33 @@ export default defineConfig({
       plugins: [['babel-plugin-react-compiler', { target: '19' }]],
     },
   },
+  routers: {
+    api: {
+      entry: './app/api.ts',
+    },
+    client: {
+      entry: './app/client.tsx',
+    },
+    ssr: {
+      entry: './app/ssr.tsx',
+    },
+  },
   server: {
     compatibilityDate: '2024-11-19',
+    esbuild: {
+      options: {
+        target: 'ES2022',
+      },
+    },
     experimental: { asyncContext: true },
     preset: 'node-server',
   },
   tsr: {
+    appDirectory: './app/',
     autoCodeSplitting: true,
+    generatedRouteTree: './app/routeTree.gen.ts',
     quoteStyle: 'single',
+    routesDirectory: './app/routes/',
     semicolons: true,
   },
   vite: {
