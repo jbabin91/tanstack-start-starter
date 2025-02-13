3db0b4472828e4c69416a125fc074f2b4df7ea77
Jace Babin <jbabin91@gmail.com>
2025-02-13 15:17:53 -0600

    feat: update auth config to allow account linking

--- app/lib/server/auth.ts
+++ app/lib/server/auth.ts
@@ -4,17 +4,21 @@ import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from '~/lib/server/db';
 
 export const auth = betterAuth({
+  account: {
+    accountLinking: {
+      enabled: true,
+      trustedProviders: ['discord', 'github'],
+    },
+  },
   baseURL: process.env.VITE_BASE_URL,
   database: drizzleAdapter(db, {
     provider: 'pg',
     usePlural: true,
   }),
-
   // https://www.better-auth.com/docs/authentication/email-password
   emailAndPassword: {
     enabled: true,
   },
-
   // https://www.better-auth.com/docs/concepts/session-management#session-caching
   session: {
     cookieCache: {
@@ -22,17 +26,16 @@ export const auth = betterAuth({
       maxAge: 5 * 60, // 5 minutes
     },
   },
-
   // https://www.better-auth.com/docs/concepts/oauth
   socialProviders: {
     discord: {
       clientId: process.env.DISCORD_CLIENT_ID!,
       clientSecret: process.env.DISCORD_CLIENT_SECRET!,
     },
-    // github: {
-    //   clientId: process.env.GITHUB_CLIENT_ID!,
-    //   clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-    // },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    },
     // google: {
     //   clientId: process.env.GOOGLE_CLIENT_ID!,
     //   clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
