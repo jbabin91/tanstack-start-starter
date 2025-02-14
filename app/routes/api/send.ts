e4fd196b0e292c46990be804905450e14706aecd
Jace Babin <jbabin91@gmail.com>
2025-02-14 12:36:39 -0600

    feat: add plain text ability for emails

--- app/routes/api/send.ts
+++ app/routes/api/send.ts
@@ -1,3 +1,4 @@
+import { render } from '@react-email/render';
 import { json } from '@tanstack/start';
 import { createAPIFileRoute } from '@tanstack/start/api';
 
@@ -6,10 +7,17 @@ import { resend } from '~/lib/server/resend';
 
 export const APIRoute = createAPIFileRoute('/api/send')({
   GET: async () => {
+    const email = EmailTemplate({ firstName: 'Jace' });
+
+    const plainText = await render(email, {
+      plainText: true,
+    });
+
     const { data, error } = await resend.emails.send({
-      from: 'TanStarter <onboarding@jacebabin.com>',
-      react: EmailTemplate({ firstName: 'Jace' }),
+      from: 'TanStarter <onboarding@starter.jacebabin.com>',
+      react: email,
       subject: 'Hello from TanStack Start',
+      text: plainText,
       to: 'jbabin91@gmail.com',
     });
 
