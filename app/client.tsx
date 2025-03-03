c2b55dd7906b5f0227b3c0a8fead287442ecbedd
Jace Babin <jbabin91@gmail.com>
2025-03-02 20:41:13 -0600

    fix: update check around react-scan

--- app/client.tsx
+++ app/client.tsx
@@ -6,9 +6,12 @@ import { hydrateRoot } from 'react-dom/client';
 
 import { createRouter } from './router';
 
-scan({
-  enabled: import.meta.env.DEV,
-});
+if (!import.meta.env.PROD) {
+  scan({
+    enabled:
+      import.meta.env.DEV && import.meta.env.VITE_ENABLE_REACT_SCAN === 'true',
+  });
+}
 
 const router = createRouter();
 
