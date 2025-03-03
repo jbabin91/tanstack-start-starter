95b9987e1eaee9b15cf1c72f16e51b68f9e24843
Jace Babin <jbabin91@gmail.com>
2025-03-02 20:52:29 -0600

    fix: try to remove react scan

--- app/client.tsx
+++ app/client.tsx
@@ -1,12 +1,8 @@
 /// <reference types="vinxi/types/client" />
-// eslint-disable-next-line simple-import-sort/imports
-import { reactScan } from './components/utils/react-scan';
 import { StartClient } from '@tanstack/react-start';
 import { hydrateRoot } from 'react-dom/client';
 
 import { createRouter } from './router';
-
-reactScan();
 
 const router = createRouter();
 
